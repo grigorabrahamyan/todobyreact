22b484944383abf027d18381f0c6f0776cae88ae
Grigor <g.r.abrahamyan@gmail.com>
2019-11-17 14:42:40 +0400

    Updates

---
+++ precache-manifest.bd75e43b103049ffae7f8eeb20ef751f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965cfdfc3bef990d33333862a7e7d30f",
+    "url": "/todobyreact/index.html"
+  },
+  {
+    "revision": "10eea0a5fca4bd4fa223",
+    "url": "/todobyreact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "325d1bce1b7e9263f7ae",
+    "url": "/todobyreact/static/js/2.7194e081.chunk.js"
+  },
+  {
+    "revision": "10eea0a5fca4bd4fa223",
+    "url": "/todobyreact/static/js/main.9f0f0a88.chunk.js"
+  },
+  {
+    "revision": "bc0da85d8c0c46e8ebb6",
+    "url": "/todobyreact/static/js/runtime-main.5b4acc6f.js"
+  }
+]);
